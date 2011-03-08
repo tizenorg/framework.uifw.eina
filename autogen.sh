41e930ef8b3ae6306403eca3a9a10bdfd31c023d
WooHyun Jung <wh0705.jung@samsung.com>
2011-03-08 12:00:36 +0900

    [Rollback]

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
+
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
@@ -7,5 +10,5 @@ echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --au
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
-	./configure -C "$@"
+	./configure "$@"
 fi
